d7207d17a73971c9afb590b6fb0f3b5850e9ac35
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-05-29 01:13:29 -0300

    aula 5.1 - salvando e recuperando dados

--- Tasky/Repository/TaskRepository.swift
+++ Tasky/Repository/TaskRepository.swift
@@ -8,21 +8,46 @@
 import Foundation
 
 class TaskRepository {
-    init() {} 
+    init() {
+        loadTasks()
+    }
     
     var tasks: [Task] = []
+    private let userDefaults = UserDefaults.standard
+    private let tasksKey = "tasky-app-tasks"
+    
+    private func saveTasks() {
+        do {
+            let tasksData = try JSONEncoder().encode(tasks)
+            userDefaults.setValue(tasksData, forKey: tasksKey)
+        } catch {
+            print("Ocorreu um erro ao salvar as tarefas: \(error)")
+        }
+    }
+    
+    private func loadTasks() {
+        guard let tasksData = userDefaults.data(forKey: tasksKey) else { return }
+        do {
+            tasks = try JSONDecoder().decode([Task].self, from: tasksData)
+        } catch {
+            print("Ocorreu um erro ao salvar as tarefas: \(error)")
+        }
+    }
     
     func addTask(newTask: Task) {
         tasks.append(newTask)
+        saveTasks()
     }
     
     func completeTask(at index: Int) {
         guard index >= 0, index < tasks.count else { return }
         tasks[index].isCompleted.toggle()
+        saveTasks()
     }
     
     func removeTask(at index: Int) {
         guard index >= 0, index < tasks.count else { return }
         tasks.remove(at: index)
+        saveTasks()
     }
 }
