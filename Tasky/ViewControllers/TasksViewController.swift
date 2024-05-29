a0bb96ee33392759df02185355ddb3a5358a7b6f
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-05-28 23:32:40 -0300

    aula 2.1 - implementando tabela de tarefas

--- Tasky/ViewControllers/TasksViewController.swift
+++ Tasky/ViewControllers/TasksViewController.swift
@@ -8,21 +8,63 @@
 import UIKit
 
 class TasksViewController: UIViewController {
-
+    
+    private var tasks: [String] = ["Fazer café", "Estudar Swift", "Fazer trabalho da faculdade"]
+    
+    private lazy var tasksTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(named: AssetsConstants.darkPurple)
+        setupNavigationBar()
+        addGradientBackground()
+        addSubviews()
+        setupConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupNavigationBar() {
+        navigationItem.hidesBackButton = true
     }
-    */
+    
+    private func addSubviews() {
+        view.addSubview(tasksTableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tasksTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tasksTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tasksTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tasksTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+}
 
+extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = tasks[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 64.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
