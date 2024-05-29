6a31b538ac5f1fbd0358f8c5f414266d7a5624b4
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-05-28 23:24:35 -0300

    aula 1.3 - tela inicial: gradiente e imagens

--- Tasky/HomeViewController.swift
+++ Tasky/HomeViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: AssetsConstants.logo))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var mainIllustrationImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: AssetsConstants.homeIllustration))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(named: AssetsConstants.darkPurple)
+        addGradientBackground()
+        addSubviews()
+        setupConstraints()
     }
-
-
+    
+    private func addSubviews() {
+        view.addSubview(logoImageView)
+        view.addSubview(mainIllustrationImageView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            mainIllustrationImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 8.0),
+            mainIllustrationImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
 
